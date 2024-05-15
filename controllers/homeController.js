ef74b4d616f6de9be02038d35c0253be1a3916d7
Benjamin Onoja <onojapeter90@gmail.com>
2024-05-15 16:43:29 +0100

    done the allJobs forEach using ejs on the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Job = require('../models/jobs')
 
 async function home (req, res) {
     try {
-        res.render('home');
+        const allJobs = await Job.find({});
+        // console.log("all jobs:", allJobs);
+        res.render('home', {allJobs});
     } catch(error) {
         console.error('Error:', error);
     }
