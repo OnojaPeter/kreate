52e18565124724cdca3b377d6367ffb8cb3d9f22
Benjamin Onoja <onojapeter90@gmail.com>
2024-05-16 23:29:54 +0100

    added the tailwind cli and removed the cdn

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,ejs}",
+  "./public/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
